93182fa4763c0c9f42790cb5a53ae099edc4f586
Illia <illia.gonya@icloud.com>
2023-04-16 15:16:52 +0200

    10 task

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-var arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let data = 0 ;
-console.log(arr);
-for (i=0; i < arr.length; i++) {
-    if (arr[i]>0) {
-        data =(data+1) * arr[i] -1;
-        
-    }
+let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+let arrMax =[];
+console.log(arr)
+for(i=0; i<arr.length; i++) { //loop for creature new array 
+    arrMax.push(arr[i]);
 }
-console.log (`Total multiply positive elements: ${data}`);
+
+arrMax.sort((a,b) => b-a); // Sorted array
+arrMax.splice(1,arrMax.length);
+
+console.log (`Only max value of array: ${arrMax}`);
