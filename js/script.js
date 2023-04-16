8345b9af474fb192afe7bd6948de65e0656047df
Illia <illia.gonya@icloud.com>
2023-04-16 15:06:20 +0200

    3 task

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let valueMin = (arr[0] || 0), serialNumber = 0; 
+let valueMax = (arr[0] || 0), serialNumber = 0; 
 for (i=0; i<arr.length; i++){
-    if (valueMin>arr[i]) {
-        valueMin = arr[i];
+    if (valueMax<arr[i]) {
+        valueMax = arr[i];
     }
 }
-serialNumber = arr.indexOf(valueMin) 
-console.log (`Minimal element: ${valueMin}\nSerial number: ${serialNumber}`);
+serialNumber = arr.indexOf(valueMax) 
+console.log (`Maximal element: ${valueMax}\nSerial number: ${serialNumber}`);
