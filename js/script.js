191e60b66e77d7307df1032ef25c092e4f270076
Illia <illia.gonya@icloud.com>
2023-04-16 15:10:31 +0200

    6 task

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ var arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,
 let counter = 0
 console.log(arr);
 for (i=0; i < arr.length; i++) {
-    if (arr[i]%2!=0 && arr[i]>0) {
+    if (arr[i]%2==0 && arr[i]>0) {
         counter++;   
     }
 }
-console.log (`Count unpaired positive element: ${counter}`);
+console.log (`Count paired positive element: ${counter}`);
