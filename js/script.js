3a9ebad5f72c41b2b9140b3cb93d6cbd994d70e9
Illia <illia.gonya@icloud.com>
2023-04-16 15:03:05 +0200

    2 task

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-var arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let counter = 0, data = 0 ;
-console.log(arr);
-for (i=0; i < arr.length; i++) {
-    if (arr[i]>0) {
-        counter++;
-        data +=arr[i];
-        
+let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+let valueMin = (arr[0] || 0), serialNumber = 0; 
+for (i=0; i<arr.length; i++){
+    if (valueMin>arr[i]) {
+        valueMin = arr[i];
     }
 }
-console.log (`Count positive element: ${counter}\nTotal sum: ${data}`);
+serialNumber = arr.indexOf(valueMin) 
+console.log (`Minimal element: ${valueMin}\nSerial number: ${serialNumber}`);
