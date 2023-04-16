98f8de22fa0089cd6166977eba251d0348dee144
Illia <illia.gonya@icloud.com>
2023-04-16 15:15:16 +0200

    9task

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,
 let data = 0 ;
 console.log(arr);
 for (i=0; i < arr.length; i++) {
-    if (arr[i]>0 && arr[i]%2!=0) {
-        data +=arr[i];
+    if (arr[i]>0) {
+        data =(data+1) * arr[i] -1;
         
     }
 }
-console.log (`Total sum unpaired positive elements: ${data}`);
+console.log (`Total multiply positive elements: ${data}`);
