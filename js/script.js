2810f26dbe820b893768afeaa0a9555f3bf88674
Illia <illia.gonya@icloud.com>
2023-04-17 10:17:19 +0200

    10 task (final)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 let arr =  [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let arrMax =[];
-console.log(arr)
-for(i=0; i<arr.length; i++) { //loop for creature new array 
-    arrMax.push(arr[i]);
-}
 
-arrMax.sort((a,b) => b-a); // Sorted array
-arrMax.splice(1,arrMax.length);
+arr.sort((a,b) => b-a); //Sort from max to min value
+arr.splice(1) //reset all value from 1 elem. of array
 
-console.log (`Only max value of array: ${arrMax}`);
+console.log (`Only max value of array: ${arr}`);
